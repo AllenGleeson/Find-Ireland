4aa0e8a1a9ab0a7850b5eeec70ab7a7b9f1b98d9
AllenGleeson <allen_gleeson@hotmail.com>
2020-04-30 15:34:40 +0000

    Added markers for irish cities

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -33,3 +33,37 @@ for (count = 0; count < locations.length; count++) {
     //  }
     //})(marker, count));
   }
+
+function initMap() {
+    var locations = [
+      ['Limerick', 52.668018, -8.630498, 4],
+      ['Galway', 53.270962, -9.062691, 5],
+      ['Cork', 51.903614, -8.468399, 3],
+      ['Dublin', 53.350140, -6.266155, 2],
+      ['Belfast', 54.607868, -5.926437, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: new google.maps.LatLng(53.350140, -6.266155),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+}      
