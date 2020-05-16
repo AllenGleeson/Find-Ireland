2fe2de3ab5c4740baeb8b86369d92efddab254c6
AllenGleeson <allen_gleeson@hotmail.com>
2020-05-16 00:04:33 +0000

    Adding html to the about.html body. A title and description of Ireland, a carousel to display some images of destinations in the country and then below some statistics on most visited locations etc. and also a weather forecast using Weather api  by weatherbit, soon to be added when I can fix a 403 error I'm getting currently.

--- wwwroot/js/weather.js
+++ wwwroot/js/weather.js
@@ -1,5 +1,7 @@
 var weatherTowns = [];
 
+var weatherapiKey = "68e04e7f0bmsh88a196efbca07adp153910jsn219512fc1d16";
+
 weatherTowns.push(towns.find(t => t.name.toLowerCase() == "belfast"));
 weatherTowns.push(towns.find(t => t.name.toLowerCase() == "dublin"));
 weatherTowns.push(towns.find(t => t.name.toLowerCase() == "galway"));
@@ -11,15 +13,30 @@ weatherTowns.forEach(town => {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": `https://weatherbit-v1-mashape.p.rapidapi.com/current?lang=en&lon=${town.long}&lat=${town.lat}`,
+        "url": `https://weatherbit-v1-mashape.p.rapidapi.com/current?key=${weatherapiKey}lang=en&lon=${town.long}&lat=${town.lat}`,
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-            "x-rapidapi-key": "68e04e7f0bmsh88a196efbca07adp153910jsn219512fc1d16"
+            "x-rapidapi-key": weatherapiKey
         }
     }
 
     $.ajax(settings).done(function (response) {
         console.log(response);
     });
+});
+
+var settings2 = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=new%20york",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "hotels4.p.rapidapi.com",
+        "x-rapidapi-key": "68e04e7f0bmsh88a196efbca07adp153910jsn219512fc1d16"
+    }
+}
+
+$.ajax(settings2).done(function (response) {
+    console.log(response);
 });
