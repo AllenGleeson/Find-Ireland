21b4202345bbcfd0ae7fb58b0061c102096d6a11
AllenGleeson <allen_gleeson@hotmail.com>
2020-05-19 16:21:25 +0000

    Got the weather api working and returning the correct results. Next Will be to display it.

--- wwwroot/js/weather.js
+++ wwwroot/js/weather.js
@@ -8,35 +8,9 @@ weatherTowns.push(towns.find(t => t.name.toLowerCase() == "galway"));
 weatherTowns.push(towns.find(t => t.name.toLowerCase() == "cork"));
 
 weatherTowns.forEach(town => {
-
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://weatherbit-v1-mashape.p.rapidapi.com/current?key=${weatherapiKey}lang=en&lon=${town.long}&lat=${town.lat}`,
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-            "x-rapidapi-key": weatherapiKey
-        }
-    }
-
-    $.ajax(settings).done(function (response) {
+    $.ajax({
+        url: `https://api.weatherbit.io/v2.0/current?lat=${town.lat}&lon=${town.long}&key=93f54e39b9cf4fb482f13d78f30ac872`
+    }).done(function (response) {
         console.log(response);
     });
-});
-
-var settings2 = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=new%20york",
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "hotels4.p.rapidapi.com",
-        "x-rapidapi-key": "68e04e7f0bmsh88a196efbca07adp153910jsn219512fc1d16"
-    }
-}
-
-$.ajax(settings2).done(function (response) {
-    console.log(response);
 });
