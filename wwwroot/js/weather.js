55a93fdf53a7b56302a4f220b006eb8e599fffa7
AllenGleeson <allen_gleeson@hotmail.com>
2020-05-20 13:50:06 +0100

    Deleted some unused icons and created a function to create the weather divs for each city.

--- wwwroot/js/weather.js
+++ wwwroot/js/weather.js
@@ -12,5 +12,29 @@ weatherTowns.forEach(town => {
         url: `https://api.weatherbit.io/v2.0/current?lat=${town.lat}&lon=${town.long}&key=93f54e39b9cf4fb482f13d78f30ac872`
     }).done(function (response) {
         console.log(response);
+        createCityWeatherForcast(town, response);
     });
 });
+
+
+function createCityWeatherForcast(town, response) {
+    let weatherForcastContainer = document.getElementsByClassName("weatherForecast");
+    let cityContainer = document.createElement('div');
+    cityContainer.className = 'col-sm-3';
+
+    let cityName = document.createElement('div');
+    cityName.innerText = town.name;
+
+    let weatherDescription = document.createElement('div');
+    weatherDescription.innerText = response.data[0].weather.description;
+
+    let weatherImage = document.createElement('img');
+    weatherImage.setAttribute("src", "./wwwroot/images/icons/weathericons/"+response.data[0].weather.icon+".png");
+    weatherImage.setAttribute("alt", response.data[0].weather.description);
+
+    cityContainer.appendChild(cityName);
+    cityContainer.appendChild(weatherDescription);
+    cityContainer.appendChild(weatherImage);
+    weatherForcastContainer[0].appendChild(cityContainer);
+
+}
